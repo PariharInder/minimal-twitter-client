0b64098af93256a67a9915bf672d5d32c4cfdcee
PariharInder <2002inderjitsingh@gmail.com>
2023-02-19 12:00:16 -0500

    added firebase routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { createBrowserRouter, RouterProvider,  } from 'react-router-dom'
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
 import Index from './pages/Index'
 import Auth from './pages/Auth'
 import './styles/skeleton.css'
@@ -7,6 +9,7 @@ import Feed from './pages/Feed'
 
 function App() {
 
+  //Initializing the router for web-app
   const router = createBrowserRouter([
     {
       path: "/",
@@ -21,6 +24,24 @@ function App() {
       element: <Feed />
     }
   ])
+
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDtZeoqPrOf-DOAw3DOzxnPSWfN6ldnATE",
+  authDomain: "minimal-twitter-d2dc2.firebaseapp.com",
+  projectId: "minimal-twitter-d2dc2",
+  storageBucket: "minimal-twitter-d2dc2.appspot.com",
+  messagingSenderId: "963857604715",
+  appId: "1:963857604715:web:fe19b211e07fed4b05d12e",
+  measurementId: "G-D8BLNNK6TC"
+}
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
   return (
     <div className="App">
