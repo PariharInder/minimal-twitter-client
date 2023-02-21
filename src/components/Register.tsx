5faecc856027a3079ccc3581c08518b037054a74
PariharInder <2002inderjitsingh@gmail.com>
2023-02-21 09:43:38 -0500

    styled register component using tailwind css

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -23,18 +23,18 @@ export const Register = () => {
     }
 
     return (
-        <div>
-            <h1>Register</h1>
-            <div>
-                <fieldset>
-                    <input type="text" placeholder="Enter username" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                </fieldset>
-                <fieldset>
-                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                </fieldset>
-                <button type="submit" onClick={handleRegister} disabled={isRegistering}>Register</button>
+        <div className=' bg-stone-900 p-6 rounded-lg shadow-xl w-5/6 lg:w-1/3 md:w-1/2 flex justify-center items-center h-full'>
+            <div className='text-center mb-10'>
+                <h1 className='text-3xl font-bold mb-8 text-white'>Register</h1>
+                <div className='p-4 mb-2'>
+                    <input className='w-full m-2 rounded-xl h-8 placeholder:text-neutral-100 bg-neutral-900 shadow-sm border-solid text-white' type="text" placeholder="Enter username" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                    <input className='w-full m-2 rounded-xl h-8 placeholder:text-neutral-100 bg-neutral-900 shadow-sm border-solid text-white' type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <div>
+                    {registrationError && <p>{registrationError}</p>}
+                    <button type="submit" onClick={handleRegister} disabled={isRegistering} className='bg-red-800 hover:bg-red-500 text-white font-bold py-2 px-4 rounded transition-colors w-50'>Sign Up</button>
+                </div>
             </div>
-            {registrationError && <p>{registrationError}</p>}
         </div>
     )
 }
